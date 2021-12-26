e9e2f8e32d879efc034faddda7c9c67fdcc806a3
Bao Nguyen <baon6052@gmail.com>
2021-12-26 15:51:46 +0000

    Add base optimizer code

---
+++ Optimizers/animation.py
@@ -0,0 +1,70 @@
+from typing import Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.animation import FuncAnimation
+
+
+def create_animation(
+    paths: list,
+    colors: list,
+    names: list,
+    func: Callable,
+    figsize: tuple = (12, 12),
+    x_lim: tuple = (-2, 2),
+    y_lim: tuple = (-1, 3),
+    n_seconds: int = 5,
+) -> FuncAnimation:
+    """
+
+    Args:
+        paths: List of arrays representing the paths (history of x,y coordinates) the optimizer went through
+        colors: List of strings representing colours for each path.
+        names: List of string representing names for each path.
+        figsize: Size of the figure.
+        x_lim: x coordinate limit of figure.
+        y_lim: y coordinate limit fo figure.
+        n_seconds: Number of seconds the animation should last for.
+        func: Function to optimize for.
+
+    Returns:
+        anim: Animation of the paths of all the optimizers.
+    """
+    if not (len(paths) == len(colors) == len(names)):
+        raise ValueError
+
+    path_length = max(len(path) for path in paths)
+
+    n_points = 300
+    x = np.linspace(*x_lim, n_points)
+    y = np.linspace(*y_lim, n_points)
+    X, Y = np.meshgrid(x, y)
+    Z = func([X, Y])
+
+    minimum = (1.0, 1.0)
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.contour(X, Y, Z, 90, cmap="jet")
+
+    scatters = [
+        ax.scatter(None, None, label=label, c=c)
+        for c, label in zip(colors, names)
+    ]
+
+    ax.legend(prop={"size": 25})
+    ax.plot(*minimum, "rD")
+
+    def animate(i):
+        for path, scatter in zip(paths, scatters):
+            scatter.set_offsets(path[:i, :])
+
+        ax.set_title(str(i))
+
+    ms_per_frame = 1000 * n_seconds / path_length
+
+    anim = FuncAnimation(
+        fig, animate, frames=path_length, interval=ms_per_frame
+    )
+
+    return anim
---
+++ Optimizers/optimization_runner.py
@@ -0,0 +1,85 @@
+from typing import Callable, Tuple
+
+import numpy as np
+import torch
+
+from torch.optim import SGD, Adam
+from tqdm import tqdm
+
+from animation import create_animation
+
+def rosenbrock(xy: Tuple) -> float:
+    """
+    Args:
+        xy: Two element tuple of floats representing the x and y coordinates.
+
+    Returns:
+        The Rosenbrock function value evaluated at the point xy.
+    """
+    x, y = xy
+    return (1 - x) ** 2 + 100 * (y - x ** 2) ** 2
+
+
+def run_optimization(
+    xy_init: Tuple,
+    optimizer_class: object,
+    n_iter: int,
+    func: Callable,
+    **optimizer_kwargs: dict
+) -> np.ndarray:
+    """Run optimization finding the minimum of a given function.
+
+    Args:
+        xy_init: Starting point - two floats representing the x and y coordinates.
+        optimizer_class: Optimizer class which we want to run.
+        n_iter: Number of iterations to run the optimization for.
+        func: Function to find the minimum against.
+        optimizer_kwargs: Additional parameters to be passed into the optimizer.
+
+    Returns:
+        path: 2D array of shape (n_iter + 1, 2). Where the rows represent
+        the iteration and the columns represent the x,y coordinates.
+    """
+    xy_t = torch.tensor(xy_init, requires_grad=True)
+    optimizer = optimizer_class([xy_t], **optimizer_kwargs)
+
+    path = np.empty((n_iter + 1, 2))
+    path[0, :] = xy_init
+
+    for i in tqdm(range(1, n_iter + 1)):
+        optimizer.zero_grad()
+        loss = func(xy_t)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm(xy_t, 1.0)
+        optimizer.step()
+
+        path[i, :] = xy_t.detach().numpy()
+
+    return path
+
+
+if __name__ == "__main__":
+    xy_init = (0.3, 0.8)
+    n_iter = 1000
+
+    path_adam = run_optimization(xy_init, Adam, n_iter, rosenbrock)
+    path_sgd = run_optimization(xy_init, SGD, n_iter, rosenbrock, lr=1e-3)
+
+    freq = 10
+
+    paths = [path_adam[::freq], path_sgd[::freq]]
+    colors = ["green", "blue"]
+    names = ["Adam", "SGD"]
+
+    anim = create_animation(
+        paths,
+        colors,
+        names,
+        rosenbrock,
+        figsize=(12, 7),
+        x_lim=(-0.1, 1.1),
+        y_lim=(-0.1, 1.1),
+        n_seconds=7,
+    )
+
+    anim.save("result.gif", writer="imagemagick")
