fc306095ca31972041665e8e77a0a9c609803d75
Bao Nguyen <baon6052@gmail.com>
2022-01-20 22:31:38 +0000

    Fix variable name conflict

--- models/generative_adversarial_networks/wasserstein_gan/data/dataset.py
+++ models/generative_adversarial_networks/wasserstein_gan/data/dataset.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 
 
 def get_loaders(batch_size: int, image_size: int, channels_img: int):
-    transforms = transforms.Compose(
+    transforms_ = transforms.Compose(
         [
             transforms.Resize(image_size),
             transforms.ToTensor(),
@@ -16,11 +16,11 @@ def get_loaders(batch_size: int, image_size: int, channels_img: int):
     )
 
     train_dataset = datasets.MNIST(
-        root="dataset/", train=True, transform=transforms, download=True
+        root="dataset/", train=True, transform=transforms_, download=True
     )
 
     test_dataset = datasets.MNIST(
-        root="dataset/", train=False, transform=transforms, download=True
+        root="dataset/", train=False, transform=transforms_, download=True
     )
 
     train_loader = DataLoader(
